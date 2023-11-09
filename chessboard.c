cad57fad5aec321e9b25877f5d20de194d8495b8
Heisenich <104042623+Heisenich@users.noreply.github.com>
2023-11-10 00:31:33 +0300

    Create chessboard.c

---
+++ chessboard.c
@@ -0,0 +1,63 @@
+void chessboard() {
+    while(front_is_clear()) {
+        step();
+// scan-beepers
+        turn_left();
+        turn_left();
+        step();
+
+        if(!beepers_present()) {
+            turn_left();
+            turn_left();
+            step();
+            put_beeper();
+        }
+        else {
+            turn_left();
+            turn_left();
+            step();
+        }
+//
+    }
+    if(facing_east()) {
+        turn_left();
+    }
+    else {
+        turn_left();
+        turn_left();
+        turn_left();
+    }
+
+    if(front_is_clear()) {
+        step();
+
+// scan-beepers
+        turn_left();
+        turn_left();
+        step();
+
+        if(!beepers_present()) {
+            turn_left();
+            turn_left();
+            step();
+            put_beeper();
+        }
+        else {
+            turn_left();
+            turn_left();
+            step();
+        }
+//
+
+        if(right_is_blocked()) {
+            turn_left();
+        }
+        else {
+            turn_left();
+            turn_left();
+            turn_left();
+        }
+
+        chessboard();
+    }
+}
